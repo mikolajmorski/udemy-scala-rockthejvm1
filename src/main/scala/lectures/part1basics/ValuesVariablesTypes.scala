912ad873918b5345887bcd1ce0be4684d0ec66fc
RSMG\mom <mikolaj.morski_extern@empolis.com>
2022-11-15 09:25:41 +0100

    2. Values, Variables and Types

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,20 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42
+  println(x)
+
+  val aString: String = "Hello, Scala"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aShort: Short = 5352
+  val aLong: Long = 2345678985634634634L
+  val aFloat: Float = 1.5f
+  val aDouble: Double = 3.14
+
+  var aVariable: Int = 4
+
+  aVariable = 10
+
+}
