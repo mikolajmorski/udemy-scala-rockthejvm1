23416fc5800ea1980c2230c8db2c590becb20d62
mmors <m.morski@gmail.com>
2022-11-25 21:54:50 +0100

    9. Smart Operations on Strings

---
+++ src/main/scala/lectures/part1basics/StringOps.scala
@@ -0,0 +1,46 @@
+package lectures.part1basics
+
+object StringOps extends App {
+
+  val str: String = "Hello I am learning scala"
+
+  println(str.charAt(2))
+  println(str.substring(7,11))
+  println(str.split(" ").toList)
+  println(str.startsWith("Hello"))
+  println(str.replace(" ", "-"))
+  println(str.toLowerCase())
+  println(str.toUpperCase())
+  println(str.length)
+
+  val aNumberStream = "42"
+  val aNumber = aNumberStream.toInt
+  println('a'+: aNumberStream :+ 'z')
+  println(str.reverse)
+  println(str.take(2))
+
+    // Scala specific: String interpolators
+
+
+  // S-interpolators
+  val name = "David"
+  val age = 12
+  val greeting = s"Hello, my name is $name and I am $age years old"
+  val anotherGreeting = s"Hello, my name is $name and I will be turning  ${age + 1} years old"
+  println(greeting)
+  println(anotherGreeting)
+
+  // F-interpolators
+  val speed = 1.2f
+  val myth = f"$name can eat $speed%2.2f burgers per minute"
+  println(myth)
+
+
+  // raw interpolator
+  println(raw"This is a \n newline")
+  val escaped = "This is a \n newline"
+  println(raw"$escaped")
+
+
+
+}
